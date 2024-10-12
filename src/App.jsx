6fc89b2f944bf5e01e30426266d57cabf11ef3c6
VihaanShah26 <vihaanshah2026@u.northwestern.edu>
2024-10-12 16:11:18 -0500

    Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ListingsPage from './ListingsPage';
 import FormComponent from './FormComponent';
+import ProfileComponent from './Profile';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         {/* Define the ListingsPage route */}
         <Route path="/listings" element={<ListingsPage />} />
         <Route path="/posting" element={<FormComponent/>}/>
+        <Route path="/profile" element={<ProfileComponent/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Profile.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import './styles/FormComponent.css';
+import { database } from './utilities/firebase';
+import { ref, set } from "firebase/database";
+
+const ProfileComponent = () => {
+  const [profileData, setProfileData] = useState({
+    firstName: '',
+    lastName: '',
+    address: '',
+    pic: null,
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProfileData({
+      ...profileData,
+      [name]: value,
+    });
+  };
+
+  const handleImageUpload = (e) => {
+    setProfileData({
+      ...profileData,
+      image: e.target.files[0],
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Should be either hash value or changed to userId later
+    const plantId = Math.floor(Math.random() * 100) + 3;
+    const dbRef = ref(database, `plants/${parseInt(plantId)}`);
+    set(dbRef, {
+      care: profileData.careDetails,
+      duration: profileData.duration,
+      favorite: false,
+      imageUrl: "/plant1.webp",
+      name: profileData.name,
+      price: 30,
+      reviews: 200,
+      rating: 4
+    })
+    // successful posting redirects user to postings
+    .then(() => {
+      window.location.href = "/listings";
+    })
+    // catch error
+    .catch((error) => {
+      console.error("Error storing data: ", error)
+    })
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h2 className="form-title">Create Your Profile</h2>
+
+      <div className="form-group">
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          id="first-name"
+          name="first-name"
+          value={profileData.firstName}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="last-name"
+          name="last-name"
+          value={profileData.lastName}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="duration">Address</label>
+        <input
+          type="text"
+          id="address"
+          name="address"
+          value={profileData.address}
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="imageUpload">Profile Picture</label>
+        <input
+          type="file"
+          id="pic"
+          accept="image/*"
+          onChange={handleImageUpload}
+        />
+        {profileData.pic && <p className="file-info">Selected file: {profileData.pic.name}</p>}
+      </div>
+
+      <button type="submit" className="submit-button">Complete</button>
+    </form>
+  );
+};
+
+export default ProfileComponent;
